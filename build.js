98087427f7a8bb253e759ac27c4484ea2a1c2675
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2015-02-18 00:57:58 +0800

    build.js

--- build.js
+++ build.js
@@ -2,13 +2,35 @@
 
 var fs = require('fs');
 var glob = require('glob');
-// var through = require('through2');
+var through = require('through2');
 var replacestream = require('replacestream');
+var marked = require('marked');
 
 glob('**/*.md', function (er, files) {
+
   files.forEach(function(file) {
+
+    var content = [];
+
     fs.createReadStream(file)
       .pipe(replacestream('.md)', '.html)'))
-      .pipe(fs.createWriteStream(file.replace('.md', '.html')));
+      .pipe(through(function(chunk, enc, callback) {
+
+        this.push(chunk);
+
+        callback();
+      }))
+      .on('data', function(data) {
+
+        content.push(data);
+      })
+      .on('end', function () {
+
+        var md = marked(content.toString());
+
+        fs.createReadStream('./template.html')
+          .pipe(replacestream('{{md}}', md))
+          .pipe(fs.createWriteStream(file.replace('.md', '.html')));
+      });
   });
 });
